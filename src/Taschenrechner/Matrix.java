ee93cba9002212f24013c62ec27e26299fe465ed
CaY-2 <cay.taugner@drvbund.onmicrosoft.com>
2025-04-28 11:30:37 +0200

    Erstellung Von Matizen und Rechenlogik

---
+++ src/Taschenrechner/Matrix.java
@@ -0,0 +1,23 @@
+package Taschenrechner;
+
+import java.util.Arrays;
+
+public abstract class Matrix {
+    protected double[][] matrix;
+
+
+    @Override
+    public String toString() {
+    String ausgabe="";
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+            ausgabe+=matrix[i][j]+"\t";
+            }
+            ausgabe+="\n";
+        }
+
+        return ausgabe;
+    }
+
+
+}
